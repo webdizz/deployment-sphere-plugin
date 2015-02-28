5cec5e15ea4a520dcc8157197c8ace5886aa1212
Izzet Mustsfaiev <webdizz@gmail.com>
2015-02-28 16:59:13 +0300

    Add Build metadata version collection functionality

--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/collector/BuildVersionMetadataCollector.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/collector/BuildVersionMetadataCollector.java
@@ -2,6 +2,7 @@ package com.epam.grandhackathon.deployment.sphere.plugin.metadata.collector;
 
 import static java.lang.String.format;
 
+import org.joda.time.DateTime;
 import hudson.model.AbstractBuild;
 import hudson.model.AbstractProject;
 import hudson.scm.ChangeLogSet;
@@ -18,7 +19,12 @@ public class BuildVersionMetadataCollector<P extends AbstractProject<P, R>, R ex
         String buildId = build.getId();
         ChangeLogSet<? extends hudson.scm.ChangeLogSet.Entry> changeSet = build.getChangeSet();
         log.fine(format("Resolved build number: %s, build id: %s, changeSet emptiness: %s", buildNumber, buildId, changeSet.isEmptySet()));
-//        return new BuildMetaData(number: buildNumber, name: build.displayName, builtAt: new DateTime(build.time.time))
-        return null;
+        BuildMetaData buildMetaData = new BuildMetaData();
+        buildMetaData.setNumber(buildNumber);
+        buildMetaData.setApplicationName("Some app name");
+        buildMetaData.setJobName(build.getDisplayName());
+        buildMetaData.setBuiltAt(new DateTime(build.due()));
+        buildMetaData.setBuildVersion(String.format("0.0.%s", buildNumber));
+        return buildMetaData;
     }
 }
--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/model/BuildMetaData.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/model/BuildMetaData.java
@@ -3,14 +3,15 @@ package com.epam.grandhackathon.deployment.sphere.plugin.metadata.model;
 import org.joda.time.DateTime;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Data
 @EqualsAndHashCode(callSuper = false)
+@ToString(callSuper = true)
 public class BuildMetaData extends ApplicationMetaData {
 
     private Integer number;
-    private String applicationName;
-    private String name;
+    private String jobName;
     private DateTime builtAt;
     private String buildVersion;
 }
