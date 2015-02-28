a90c0157d7c6486b1e1649b38c3072efb7189c26
Tair Mustafaiev <The>
2015-02-28 19:12:03 +0300

    added loop

--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/SphereDeployBoardAction.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/SphereDeployBoardAction.java
@@ -1,16 +1,19 @@
 package com.epam.grandhackathon.deployment.sphere.plugin;
 
-
-import javax.annotation.Nonnull;
-
-import org.acegisecurity.AccessDeniedException;
-import org.kohsuke.stapler.export.ExportedBean;
+import com.epam.grandhackathon.deployment.sphere.plugin.metadata.model.EnvironmentMetaData;
+import com.epam.grandhackathon.deployment.sphere.plugin.service.EnvironmentDataServiceImpl;
 import hudson.Extension;
 import hudson.model.RootAction;
 import hudson.security.ACL;
 import hudson.security.AccessControlled;
 import hudson.security.Permission;
 import jenkins.model.Jenkins;
+import org.acegisecurity.AccessDeniedException;
+import org.kohsuke.stapler.export.Exported;
+import org.kohsuke.stapler.export.ExportedBean;
+
+import javax.annotation.Nonnull;
+import java.util.List;
 
 @ExportedBean(defaultVisibility = 999)
 @Extension
@@ -29,6 +32,11 @@ public class SphereDeployBoardAction implements RootAction, AccessControlled {
     @Override
     public String getUrlName () {
         return "/deployment-sphere";
+    }
+
+    @Exported
+    public List<EnvironmentMetaData> getEnvironments() {
+        return new EnvironmentDataServiceImpl().getAllEnvironments();
     }
 
     @Nonnull
