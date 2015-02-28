60cab0d3c5ddaf940666ef2706fe824566f3bbdc
Fevzi Anifieiev <Fevzi_Anifieiev@epam.com>
2015-02-28 23:44:15 +0200

    [Hackathon2015]-buildVersion auto resolving

--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/BuildVersionMetaDataPublisher.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/BuildVersionMetaDataPublisher.java
@@ -1,28 +1,28 @@
 package com.epam.grandhackathon.deployment.sphere.plugin;
 
-import static java.lang.String.format;
 import static com.google.common.base.Preconditions.checkArgument;
+import static java.lang.String.format;
+import hudson.Launcher;
+import hudson.model.BuildListener;
+import hudson.model.AbstractBuild;
+import hudson.tasks.BuildStepMonitor;
+import hudson.tasks.Notifier;
 
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.logging.Level;
+
+import lombok.extern.java.Log;
 
 import org.kohsuke.stapler.DataBoundConstructor;
 import org.kohsuke.stapler.DataBoundSetter;
 
-import hudson.EnvVars;
-import hudson.Launcher;
-import hudson.model.AbstractBuild;
-import hudson.model.BuildListener;
-import hudson.tasks.BuildStepMonitor;
-import hudson.tasks.Notifier;
-import lombok.extern.java.Log;
-
+import com.epam.grandhackathon.deployment.sphere.plugin.action.DynamicVariablesStorageAction;
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.Constants;
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.collector.BuildVersionMetaDataCollector;
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.model.BuildMetaData;
-import com.epam.grandhackathon.deployment.sphere.plugin.utils.EnvVarsExtractor;
+import com.epam.grandhackathon.deployment.sphere.plugin.utils.EnvVarsResolver;
 import com.google.common.base.Strings;
-import com.google.common.base.Throwables;
 
 @Log
 public class BuildVersionMetaDataPublisher extends Notifier {
@@ -51,9 +51,9 @@ public class BuildVersionMetaDataPublisher extends Notifier {
         checkArgument(null != listener, "Listener is null, something was wrong.");
         checkArgument(null != build, "Current build is null, something was wrong.");
 
-        listener.getLogger().append("[deployment-sphere] Collecting build metadata\n");
-
         BuildMetaData buildMetaData = new BuildVersionMetaDataCollector().collect(build, listener);
+
+        listener.getLogger().append("[deployment-sphere] Collecting build metadata\n").append(buildMetaData.toString());
         log.log(Level.FINEST, format("Next build metadata was collected %s", buildMetaData));
 
         return true;
@@ -64,10 +64,18 @@ public class BuildVersionMetaDataPublisher extends Notifier {
         final String pattern = getVersionPattern();
         checkArgument(!Strings.isNullOrEmpty(pattern), "Build Pattern version value must be provided");
 
+        PrintStream logger = listener.getLogger();
+        final String thisClassName = getClass().getName();
+
+        logger.append(String.format("[%s]Build type: %s\n", thisClassName, build.getClass().getName()));
+        logger.append(String.format("[%s]Build instance: %s\n", thisClassName, build));
+
+        logger.append(String.format("[%s]Build Listener type: %s\n", thisClassName, listener.getClass().getName()));
+        logger.append(String.format("[%s]Build Listener instance: %s\n", thisClassName, listener));
+
         final String buildVersion = pattern.replace("{v}", String.valueOf(build.getNumber()));
 
-        final EnvVarsExtractor envExtractor = new EnvVarsExtractor(build, listener);
-        envExtractor.putValue(Constants.BUILD_VERSION, buildVersion);
+        build.addAction(new DynamicVariablesStorageAction(Constants.BUILD_VERSION, buildVersion));
 
         return true;
     }
--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/utils/EnvVarsResolver.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/utils/EnvVarsResolver.java
@@ -1,5 +1,9 @@
 package com.epam.grandhackathon.deployment.sphere.plugin.utils;
 
+import hudson.EnvVars;
+import hudson.model.TaskListener;
+import hudson.model.AbstractBuild;
+
 import java.io.IOException;
 import java.util.logging.Level;
 
@@ -7,31 +11,24 @@ import lombok.extern.java.Log;
 
 import com.google.common.base.Throwables;
 
-import hudson.EnvVars;
-import hudson.model.TaskListener;
-import hudson.model.AbstractBuild;
-
 @Log
-public final class EnvVarsExtractor {
+public final class EnvVarsResolver {
     private final AbstractBuild<?, ?> build;
     private final TaskListener taskListener;
 
-    public EnvVarsExtractor(final AbstractBuild<?, ?> build, final TaskListener taskListener) {
+    public EnvVarsResolver(final AbstractBuild<?, ?> build, final TaskListener taskListener) {
         this.build = build;
         this.taskListener = taskListener;
     }
 
     public String getValue(final String key) {
-        return extract().get(key);
+        EnvVars env = getEnvVars(this.build, this.taskListener);
+        return env.get(key);
     }
 
-    public void putValue(final String key, final String value) {
-        extract().put(key, value);
-    }
-
-    public EnvVars extract() {
+    private static EnvVars getEnvVars(final AbstractBuild<?, ?> build, final TaskListener taskListener) {
         try {
-            return build.getEnvironment(this.taskListener);
+            return build.getEnvironment(taskListener);
         } catch (final IOException | InterruptedException exc) {
             log.log(Level.SEVERE, "Failed getting env variables", exc);
             throw Throwables.propagate(exc);
