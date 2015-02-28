003677c163677830cdba3715dbc159f9c694d1f8
Izzet Mustsfaiev <webdizz@gmail.com>
2015-02-28 14:02:04 +0300

    The first hello version of build metadata collector functionality

--- deployment-sphere-plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/collector/BuildVersionMetadataCollectorDescriptor.java
+++ deployment-sphere-plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/collector/BuildVersionMetadataCollectorDescriptor.java
@@ -1,22 +1,29 @@
 package com.epam.grandhackathon.deployment.sphere.plugin.collector;
 
+import java.util.logging.Level;
+
 import hudson.Extension;
 import hudson.model.AbstractProject;
 import hudson.tasks.BuildStepDescriptor;
 import hudson.tasks.Publisher;
 import jenkins.YesNoMaybe;
+import lombok.extern.java.Log;
 
+@Log
 @Extension(dynamicLoadable = YesNoMaybe.YES)
 public class BuildVersionMetadataCollectorDescriptor extends BuildStepDescriptor<Publisher> {
 
     public BuildVersionMetadataCollectorDescriptor() {
         super(BuildVersionMetadataPublisher.class);
+        load();
     }
 
     @Override
     public boolean isApplicable(final Class<? extends AbstractProject> jobType) {
+        log.log(Level.FINE, "Current project is of type %s", jobType);
         return true;
     }
+
 
     public String getDisplayName() {
         return "Collect Build Metadata";
--- deployment-sphere-plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/collector/BuildVersionMetadataPublisher.java
+++ deployment-sphere-plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/collector/BuildVersionMetadataPublisher.java
@@ -1,10 +1,30 @@
 package com.epam.grandhackathon.deployment.sphere.plugin.collector;
 
-import hudson.tasks.BuildStepMonitor;
+import java.io.IOException;
+import java.util.logging.Level;
 
+import org.kohsuke.stapler.DataBoundConstructor;
+import hudson.Launcher;
+import hudson.model.AbstractBuild;
+import hudson.model.BuildListener;
+import hudson.tasks.BuildStepMonitor;
+import lombok.extern.java.Log;
+
+@Log
 public class BuildVersionMetadataPublisher extends hudson.tasks.Notifier {
+
+    @DataBoundConstructor
+    public BuildVersionMetadataPublisher() {
+    }
+
     @Override
     public BuildStepMonitor getRequiredMonitorService() {
         return BuildStepMonitor.BUILD;
+    }
+
+    @Override
+    public boolean perform(final AbstractBuild<?, ?> build, final Launcher launcher, final BuildListener listener) throws InterruptedException, IOException {
+        log.log(Level.FINE, "Is about to collect build metadata");
+        return true;
     }
 }
