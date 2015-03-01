b1b0a0629ffae45caa6006d5224fa97d4dd6a811
Izzet Mustsfaiev <webdizz@gmail.com>
2015-03-01 03:36:04 +0300

    Fix compilation errors

--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/persistence/domain/Environment.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/persistence/domain/Environment.java
@@ -1,16 +1,12 @@
 package com.epam.grandhackathon.deployment.sphere.plugin.metadata.persistence.domain;
 
 import java.io.Serializable;
-import java.util.List;
 
 import lombok.Data;
-
 
 @Data
 public class Environment implements Serializable {
     private String key;
     private String title;
-
-    private List<Deployment> deployments;
 
 }
--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/persistence/query/EnvironmentQuery.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/persistence/query/EnvironmentQuery.java
@@ -1,28 +1,37 @@
 package com.epam.grandhackathon.deployment.sphere.plugin.metadata.persistence.query;
 
-import com.epam.grandhackathon.deployment.sphere.plugin.metadata.persistence.domain.Environment;
-import org.skife.jdbi.v2.SQLStatement;
-import org.skife.jdbi.v2.StatementContext;
-import org.skife.jdbi.v2.sqlobject.*;
-import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
-import org.skife.jdbi.v2.tweak.ResultSetMapper;
-
-import java.lang.annotation.*;
+import java.lang.annotation.Annotation;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
+import org.skife.jdbi.v2.SQLStatement;
+import org.skife.jdbi.v2.StatementContext;
+import org.skife.jdbi.v2.sqlobject.Bind;
+import org.skife.jdbi.v2.sqlobject.Binder;
+import org.skife.jdbi.v2.sqlobject.BindingAnnotation;
+import org.skife.jdbi.v2.sqlobject.SqlQuery;
+import org.skife.jdbi.v2.sqlobject.SqlUpdate;
+import org.skife.jdbi.v2.sqlobject.customizers.RegisterMapper;
+import org.skife.jdbi.v2.tweak.ResultSetMapper;
+
+import com.epam.grandhackathon.deployment.sphere.plugin.metadata.persistence.domain.Environment;
+
 @RegisterMapper(EnvironmentQuery.Mapper.class)
 public interface EnvironmentQuery {
 
-    @SqlQuery("SELECT * FROM ENVIRONMENTS WHERE identity = :identity AND title = :title")
-    Environment find (@Bind("identity") String applicationName, @Bind("title") String title);
+    @SqlQuery("SELECT * FROM ENVIRONMENTS WHERE key = :key AND title = :title")
+    Environment find(@Bind("key") String applicationName, @Bind("title") String title);
 
-    @SqlUpdate("INSERT INTO ENVIRONMENTS (identity, title) values (:identity, :identity)")
-    int save (@BindEnvironment Environment build);
+    @SqlUpdate("INSERT INTO ENVIRONMENTS (key, title) values (:key, :title)")
+    int save(@BindEnvironment Environment build);
 
     @SqlQuery("SELECT * FROM ENVIRONMENTS")
-    List<Environment> all ();
+    List<Environment> all();
 
     @BindingAnnotation(BindEnvironment.BinderFactory.class)
     @Retention(RetentionPolicy.RUNTIME)
@@ -30,11 +39,11 @@ public interface EnvironmentQuery {
     @interface BindEnvironment {
 
         class BinderFactory implements org.skife.jdbi.v2.sqlobject.BinderFactory {
-            public Binder build (Annotation annotation) {
+            public Binder build(Annotation annotation) {
                 return new Binder<BindEnvironment, Environment>() {
                     @Override
-                    public void bind (final SQLStatement<?> q, final BindEnvironment bind, final Environment arg) {
-                        q.bind("identity", arg.getIdentity());
+                    public void bind(final SQLStatement<?> q, final BindEnvironment bind, final Environment arg) {
+                        q.bind("key", arg.getKey());
                         q.bind("title", arg.getTitle());
                     }
                 };
@@ -44,11 +53,11 @@ public interface EnvironmentQuery {
 
     class Mapper implements ResultSetMapper<Environment> {
         @Override
-        public Environment map (final int index, final ResultSet resultSet, final StatementContext ctx) throws
+        public Environment map(final int index, final ResultSet resultSet, final StatementContext ctx) throws
                 SQLException {
             Environment build = new Environment();
             build.setTitle(resultSet.getString("title"));
-            build.setIdentity(resultSet.getString("identity"));
+            build.setKey(resultSet.getString("key"));
             return build;
         }
     }
