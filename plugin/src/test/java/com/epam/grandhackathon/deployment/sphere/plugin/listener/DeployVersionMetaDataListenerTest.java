f68af8e2c46d82be1113f0d37022c678591f801f
Roman Kovalenko <rkovalenko@datas-tech.com.ua>
2015-04-10 21:57:47 +0300

    Fixed test case for plugin listener

--- plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/listener/DeployVersionMetaDataListenerTest.java
+++ plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/listener/DeployVersionMetaDataListenerTest.java
@@ -5,6 +5,7 @@ import hudson.model.FreeStyleProject;
 import hudson.model.ParameterDefinition;
 import hudson.model.ParametersDefinitionProperty;
 
+import lombok.extern.java.Log;
 import org.junit.Rule;
 import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
@@ -14,33 +15,36 @@ import org.slf4j.LoggerFactory;
 import com.epam.grandhackathon.deployment.sphere.plugin.DeployVersionMetaDataPublisher;
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.Constants;
 
+@Log
 public class DeployVersionMetaDataListenerTest {
-	private static final Logger LOGGER = LoggerFactory.getLogger(DeployVersionMetaDataListenerTest.class);
-    
 	@Rule
     public JenkinsRule j = new JenkinsRule();
 	
 	@Test
 	public void shouldCreateConfigParameterOnCreate() throws Exception {
-		LOGGER.info("Create project with Deployment Sphere plugin");
-		assertTrue(checkProjectConfigurationWithPlugin());
-		LOGGER.info("Done.");
+		log.info("Create project with Deployment Sphere plugin");
+		assertTrue(checkProjectConfigurationWithPlugin(true));
+        log.info("Done.");
 		
 	}
 
 	@Test
 	public void shouldCreateConfigParameterOnUpdate() throws Exception{
-		LOGGER.info("Update project with Deployment Sphere plugin");
-		assertTrue(checkProjectConfigurationWithPlugin());
-		LOGGER.info("Done.");	
+        log.info("Update project with Deployment Sphere plugin");
+		assertTrue(checkProjectConfigurationWithPlugin(false));
+        log.info("Done.");
 	}
 	
-	private boolean checkProjectConfigurationWithPlugin() throws Exception{
+	private boolean checkProjectConfigurationWithPlugin(boolean isCreate) throws Exception{
 		FreeStyleProject project = j.createFreeStyleProject("Deployment job");
 		project.getPublishersList().add(new DeployVersionMetaDataPublisher());
 		project.save();
 		DeployVersionMetaDataListener listener = new DeployVersionMetaDataListener();
-		listener.onCreated(project);
+		if (isCreate){
+            listener.onCreated(project);
+        }else{
+            listener.onUpdated(project);
+        }
 		ParametersDefinitionProperty paramDefProp = (ParametersDefinitionProperty)project.getProperty(ParametersDefinitionProperty.class);
 		if (null != paramDefProp){
 			for(ParameterDefinition pDef : paramDefProp.getParameterDefinitions()){
