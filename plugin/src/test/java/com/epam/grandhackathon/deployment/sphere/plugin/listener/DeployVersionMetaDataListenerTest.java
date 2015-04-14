24a8f10f02e0edc5b5aff78a0d50a3acb885d799
Roman Kovalenko <rkovalenko@datas-tech.com.ua>
2015-04-14 20:35:51 +0300

    Fixed test case for plugin listener

--- plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/listener/DeployVersionMetaDataListenerTest.java
+++ plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/listener/DeployVersionMetaDataListenerTest.java
@@ -11,50 +11,47 @@ import lombok.extern.java.Log;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.jvnet.hudson.test.JenkinsRule;
 
 import static org.junit.Assert.assertTrue;
 
 @Log
 public class DeployVersionMetaDataListenerTest {
-	@Rule
-    public PluginJenkinsRule j = new PluginJenkinsRule();
-	
-	private boolean origDefaultUseCache = true;
-	 
-	@Test
-	public void shouldCreateConfigParameterOnCreate() throws Exception {
-		log.info("Create project with Deployment Sphere plugin");
-		assertTrue(checkProjectConfigurationWithPlugin(true));
-        log.info("Done.");
-		
-	}
+    @Rule
+    public PluginJenkinsRule jenkinsRule = new PluginJenkinsRule();
 
-	@Test
-	public void shouldCreateConfigParameterOnUpdate() throws Exception{
-        log.info("Update project with Deployment Sphere plugin");
-		assertTrue(checkProjectConfigurationWithPlugin(false));
+    @Test
+    public void shouldCreateConfigParameterOnCreate() throws Exception {
+        log.info("Create project with Deployment Sphere plugin");
+        assertTrue(checkProjectConfigurationWithPlugin(true));
         log.info("Done.");
-	}
-	
-	private boolean checkProjectConfigurationWithPlugin(boolean isCreate) throws Exception{
-		FreeStyleProject project = j.createFreeStyleProject("Deployment job");
-		project.getPublishersList().add(new DeployVersionMetaDataPublisher());
-		project.save();
-		DeployVersionMetaDataListener listener = new DeployVersionMetaDataListener();
-		if (isCreate){
+
+    }
+
+    @Test
+    public void shouldCreateConfigParameterOnUpdate() throws Exception {
+        log.info("Update project with Deployment Sphere plugin");
+        assertTrue(checkProjectConfigurationWithPlugin(false));
+        log.info("Done.");
+    }
+
+    private boolean checkProjectConfigurationWithPlugin(boolean isCreate) throws Exception {
+        FreeStyleProject project = jenkinsRule.createFreeStyleProject("Deployment job");
+        project.getPublishersList().add(new DeployVersionMetaDataPublisher());
+        project.save();
+        DeployVersionMetaDataListener listener = new DeployVersionMetaDataListener();
+        if (isCreate) {
             listener.onCreated(project);
-        }else{
+        } else {
             listener.onUpdated(project);
         }
-		ParametersDefinitionProperty paramDefProp = (ParametersDefinitionProperty)project.getProperty(ParametersDefinitionProperty.class);
-		if (null != paramDefProp){
-			for(ParameterDefinition pDef : paramDefProp.getParameterDefinitions()){
-				if (pDef.getName().equals(Constants.DEPLOY_META_DATA)){
-					return true;
-				}
-			}
-		}
-		return false;
-	}
+        ParametersDefinitionProperty paramDefProp = (ParametersDefinitionProperty) project.getProperty(ParametersDefinitionProperty.class);
+        if (null != paramDefProp) {
+            for (ParameterDefinition pDef : paramDefProp.getParameterDefinitions()) {
+                if (pDef.getName().equals(Constants.DEPLOY_META_DATA)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
 }
