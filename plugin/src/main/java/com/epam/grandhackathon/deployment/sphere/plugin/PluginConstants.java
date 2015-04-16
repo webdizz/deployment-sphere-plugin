426efc435c1ba353be72ebbef2f44d77cadd5452
Roman Kovalenko <romko.kovalenko@gmail.com>
2015-04-16 14:38:19 +0300

    - Changed plugin db file location to plugin_home - Removed plugin dependency - com.cloudbees.plugins:build-flow-plugin

--- plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/PluginConstants.java
+++ plugin/src/main/java/com/epam/grandhackathon/deployment/sphere/plugin/PluginConstants.java
@@ -6,4 +6,5 @@ public class PluginConstants {
     public static final String DEPLOY_JOB_PARAMETER = "Deploy meta data parameter";
     public static final String BUILD_METADATA_COLLECTOR_NAME = "Collect Build Metadata";
     public static final String DEPLOY_METADATA_COLLECTOR_NAME = "Collect Deploy Metadata";
+    public static final String PLUGIN_CONTEXT = "deployment-sphere";
 }
