32a9566888dbfaa122f1e4d1a9e7e07010bacf08
Oleksii_Zaitsev <Oleksii_Zaitsev@epam.com>
2015-04-16 14:07:22 +0300

    Fix test format

--- plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/collector/BuildVersionMetaDataCollectorTest.java
+++ plugin/src/test/java/com/epam/grandhackathon/deployment/sphere/plugin/metadata/collector/BuildVersionMetaDataCollectorTest.java
@@ -11,11 +11,11 @@ import java.io.PrintStream;
 import java.util.Calendar;
 import java.util.TreeMap;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.experimental.theories.DataPoints;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
-import org.mockito.runners.MockitoJUnitRunner;
 
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.Constants;
 import com.epam.grandhackathon.deployment.sphere.plugin.metadata.model.BuildMetaData;
@@ -26,60 +26,54 @@ import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-@RunWith(MockitoJUnitRunner.class)
+@RunWith(Theories.class)
 public class BuildVersionMetaDataCollectorTest {
-	
-	private static final String BUILD_VERSION = "1.2";
-	private static final Integer BUILD_NUMBER = 1;
-	private static final String BUILD_ID = "buildId";
-	private static final String JOB_DISPLAY_NAME = "jobName";
-	private static final String BUILD_APP_NAME = "app-name";
 
-	private Injector injector;
-	private BuildVersionMetaDataCollector metaDataCollector;
-	
-	private AbstractBuild<?, ?> build;
-	private TaskListener listener;
-	
-	@Before
-	public void setUp() throws Exception {
-		injector = Guice.createInjector(new AbstractModule() {
-            @Override
-            protected void configure() {
-            	bind(BuildMetaDataDao.class).to(BuildMetaDataDaoMock.class);
-            }
-        });
-		initBuild();
-		
-		listener = Mockito.mock(TaskListener.class);
-		Mockito.when(listener.getLogger()).thenReturn(new PrintStream(System.out));
-		
-		metaDataCollector = injector.getInstance(BuildVersionMetaDataCollector.class);
-	}
+	@DataPoints
+    public static final String[][] testData = new String [][]{
+		{"1", "buildId",  "jobName", "1.2", "app-name", "http://test.com/test"}
+    };
 
-	private void initBuild(){
-		build = Mockito.mock(AbstractBuild.class);
-		Mockito.when(build.getNumber()).thenReturn(BUILD_NUMBER);
-		Mockito.when(build.getId()).thenReturn(BUILD_ID);
+	private AbstractBuild<?, ?> initBuild(String[] testData){
+		AbstractBuild<?, ?> build = Mockito.mock(AbstractBuild.class);
+		Mockito.when(build.getNumber()).thenReturn(Integer.parseInt(testData[0]));
+		Mockito.when(build.getId()).thenReturn(testData[1]);
 		Mockito.when(build.getChangeSet()).thenReturn(new EmptyChangeLogSet(build));
-		Mockito.when(build.getDisplayName()).thenReturn(JOB_DISPLAY_NAME);
+		Mockito.when(build.getDisplayName()).thenReturn(testData[2]);
 		Mockito.when(build.due()).thenReturn(Calendar.getInstance());
 		TreeMap<String, String> treeMap = new TreeMap<String,String>();
-		treeMap.put(Constants.BUILD_VERSION, BUILD_VERSION);
-		treeMap.put(Constants.BUILD_APP_NAME, BUILD_APP_NAME);
+		treeMap.put(Constants.BUILD_VERSION, testData[3]);
+		treeMap.put(Constants.BUILD_APP_NAME, testData[4]);
 		try {
 			Mockito.when(build.getEnvironment(any(TaskListener.class))).thenReturn(new EnvVars(treeMap));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		return build;
+	}
+	private BuildVersionMetaDataCollector getMetaDataCollectorWithMocks() {
+		Injector injector = Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+            	bind(BuildMetaDataDao.class).to(BuildMetaDataDaoMock.class);
+            }
+        });
+		BuildVersionMetaDataCollector metaDataCollector = injector.getInstance(BuildVersionMetaDataCollector.class);
+		return metaDataCollector;
 	}
 	
-	@Test
-	public void testCollect() {
-		String TEST_URL = "http://test.com/test";
-		Mockito.when(build.getUrl()).thenReturn(TEST_URL);
+	@Theory
+	public void shouldGetUrlFromBuildAntPutInBuildMetaDataUrl(String[] testData) {
+		BuildVersionMetaDataCollector metaDataCollector = getMetaDataCollectorWithMocks();
+		AbstractBuild<?, ?> build = initBuild(testData);
+		
+		Mockito.when(build.getUrl()).thenReturn(testData[5]);
+		
+		TaskListener listener = Mockito.mock(TaskListener.class);
+		Mockito.when(listener.getLogger()).thenReturn(new PrintStream(System.out));
 		
 		BuildMetaData result = metaDataCollector.collect(build, listener);
-		assertThat(result.getBuildUrl(),is(TEST_URL));
+		assertThat(result.getBuildUrl(),is(testData[5]));
 	}
+	
 }
