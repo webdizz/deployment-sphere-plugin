11cece7459d5b0417641816a8e4550c8fac2969b
Izzet Mustsfaiev <webdizz@gmail.com>
2015-05-07 22:30:32 +0300

    Fix typos

--- plugin/src/main/java/com/epam/jenkins/deployment/sphere/plugin/config/Environment.java
+++ plugin/src/main/java/com/epam/jenkins/deployment/sphere/plugin/config/Environment.java
@@ -1,36 +1,34 @@
 package com.epam.jenkins.deployment.sphere.plugin.config;
 
+import org.kohsuke.stapler.DataBoundConstructor;
 import hudson.Extension;
 import hudson.model.Describable;
 import hudson.model.Descriptor;
 
-import org.kohsuke.stapler.DataBoundConstructor;
-
 public class Environment implements Describable<Environment> {
-	@Extension
-	public final static DescriptorImpl DESCRIPOR = new DescriptorImpl();
-	private String title;
+    @Extension
+    public final static DescriptorImpl DESCRIPTOR = new DescriptorImpl();
+    private String title;
 
-	public static class DescriptorImpl extends Descriptor<Environment> {
+    public static class DescriptorImpl extends Descriptor<Environment> {
 
-		@Override
-		public String getDisplayName() {
-			return "Enviroment";
-		}
-	};
+        @Override
+        public String getDisplayName() {
+            return "Environment";
+        }
+    }
 
+    @DataBoundConstructor
+    public Environment(String title) {
+        this.title = title;
+    }
 
-	@DataBoundConstructor
-	public Environment(String title) {
-		this.title = title;
-	}
+    public String getTitle() {
+        return title;
+    }
 
-	public String getTitle() {
-		return title;
-	}
-
-	@Override
-	public Descriptor<Environment> getDescriptor() {
-		return DESCRIPOR;
-	}
+    @Override
+    public Descriptor<Environment> getDescriptor() {
+        return DESCRIPTOR;
+    }
 }
